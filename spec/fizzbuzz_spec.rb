fc006b7c2ae369fad994333273bdc21fc2cc47fa
Jack <92jackhenderson@gmail.com>
2016-12-13 14:06:49 +0000

    speccy fizzybuzz

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,23 @@
+require './lib/fizzbuzz'
+
+describe 'fizzbuzz' do 
+    it 'returns "fizzbuzz" when passed a multiple of 3 and 5' do 
+            expect(fizzbuzz(15)).to eq 'fizzbuzz'
+            expect(fizzbuzz(30)).to eq 'fizzbuzz'
+    end
+    
+    it 'returns "fizz" when passed a multiple of 3' do 
+            expect(fizzbuzz(24)).to eq 'fizz'
+            expect(fizzbuzz(18)).to eq 'fizz'
+    end
+
+    it 'returns "buzz" when passed a multiple of 5' do
+            expect(fizzbuzz(5)).to eq 'buzz'
+            expect(fizzbuzz(25)).to eq 'buzz'
+    end
+    
+    it 'returns same number when passed none of the above' do
+            expect(fizzbuzz(2)).to eq 2
+            expect(fizzbuzz(11)).to eq 11
+    end
+end 
