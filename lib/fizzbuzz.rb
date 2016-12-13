7ab8e5e96545a3fc1d5fd2e315c1a4ee2d0586e4
Jack <92jackhenderson@gmail.com>
2016-12-13 14:31:20 +0000

    selfie method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,28 @@
+
 def fizzbuzz(number)
-    if number % 5 == 0 && number % 3 == 0
-        "fizzbuzz"
-    elsif number % 5 == 0
-        "buzz"
-    elsif number % 3 == 0
-        "fizz"
-    else 
-        number
+        if number % 5 == 0 && number % 3 == 0
+            return "fizzbuzz"
+        elsif number % 5 == 0
+            return "buzz"
+        elsif number % 3 == 0
+            return "fizz"
+        else 
+           return number
+        end
+    end
+
+
+class ::Fixnum
+
+    def fizzbuzz_zz
+        if self % 5 == 0 && self % 3 == 0
+            return "fizzbuzz"
+        elsif self % 5 == 0
+            return "buzz"
+        elsif self % 3 == 0
+            return "fizz"
+        else 
+           return self
+        end
     end
 end
